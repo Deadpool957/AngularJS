313df203f3a7848378d9c5b9d0534199ea52ec51
linsheng <986992484@qq.com>
2017-01-15 22:28:37 +0800

    指令与指令的交互

--- Directive/Directive&Directive/Directive&Directive.js
+++ Directive/Directive&Directive/Directive&Directive.js
@@ -3,7 +3,11 @@ myModule.directive("superman", function() {
     return {
         scope: {},  //创建独立的作用域
         restrict: 'AE',
-        controller: function($scope) {
+        controller: function($scope) {  
+       /*
+         这里的controller和MVC里的controller不一样，这里的controller是为了暴露出内部的方法，
+         以便外部调用。指令和指令之间的交互就是靠这个controller来实现的。
+       */
             $scope.abilities = [];
             this.addStrength = function() {
                 $scope.abilities.push("strength");
@@ -15,8 +19,8 @@ myModule.directive("superman", function() {
                 $scope.abilities.push("light");
             };
         },
+        /*link是绑定事件，内部用的，处理内部逻辑*/
         link: function(scope, element, attrs) {
-            element.addClass('btn btn-primary');
             element.bind("mouseenter", function() {
                 console.log(scope.abilities);
             });
@@ -25,8 +29,9 @@ myModule.directive("superman", function() {
 });
 myModule.directive("strength", function() {
     return {
-        require: '^superman',
-        link: function(scope, element, attrs, supermanCtrl) {
+        require: '^superman',  // require表示依赖于
+        link: function(scope, element, attrs, supermanCtrl) {  
+       //有了依赖，就可以写第四个参数了，第四个参数其实就是上面的controller
             supermanCtrl.addStrength();
         }
     }
