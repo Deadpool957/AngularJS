8b05f351294642d4e59f0752efc3e0db45cf915b
linsheng <986992484@qq.com>
2017-01-15 11:31:41 +0800

    BookStore

--- Both-Data-bind/BookStore/app/js/app.js
+++ Both-Data-bind/BookStore/app/js/app.js
@@ -1,8 +1,10 @@
+/*这里第一个参数是模块的名称，第二个参数是相关的依赖*/
 var bookStoreApp = angular.module('bookStoreApp', [
     'ngRoute', 'ngAnimate', 'bookStoreCtrls', 'bookStoreFilters',
     'bookStoreServices', 'bookStoreDirectives'
 ]);
 
+/*配置，使用$routeProvider来配置路由*/
 bookStoreApp.config(function($routeProvider) {
     $routeProvider.when('/hello', {
         templateUrl: 'tpls/hello.html',
