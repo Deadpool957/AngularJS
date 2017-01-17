613f0f1eb7263bae65e813d35be0471d3ee325c1
linsheng <986992484@qq.com>
2017-01-17 22:31:35 +0800

    表单验证

---
+++ Form-validate/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 钟林生 on 2017/1/17.
+ */
+var myApp=angular.module('myApp',[]);
+myApp.controller('mainController',function ($scope) {
+    $scope.submitForm=function () {
+        console.log('dfr');
+    }
+});
